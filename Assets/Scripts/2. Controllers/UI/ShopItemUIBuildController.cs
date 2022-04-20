3b5a03eef5e841d87659cc9241a3fa38d8e874e5
Ace Derrick Young <luadengames@gmail.com>
2022-04-20 19:52:32 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/2. Controllers/UI/ShopItemUIBuildController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItemUIBuildController : MonoBehaviour
+{
+    [SerializeField] private GameObject ItemPrefab;
+    public void BuildAndDisplayItemUI(ShopItemUIObject shopItem, Transform startPoint)
+    {
+        GameObject shopItemUIGameObject;
+        shopItemUIGameObject = Instantiate(ItemPrefab, transform);
+        shopItemUIGameObject.transform.position = startPoint.position;
+
+        shopItem.ShopItemUIController = shopItemUIGameObject;
+        ComponentShopVendorUIController componentShopUIController = shopItemUIGameObject.GetComponent<ComponentShopVendorUIController>();
+
+        componentShopUIController.InitUI(shopItem);
+        DowntimeManager.Instance.ComponentShopManager.ComponentShopVendorSlotManager.AddItemToCollection(componentShopUIController, null);
+
+        shopItemUIGameObject.SetActive(true);
+    }
+}
