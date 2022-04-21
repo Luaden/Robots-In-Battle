3e4c1bbfbf7ab8a27b60d5d012ccc0b822cc10fd
Ace Derrick Young <luadengames@gmail.com>
2022-04-21 22:05:43 +0200

    Bug Fix / Console Narration Update

--- Assets/Scripts/1. Managers/UI/BuffUIManager.cs
+++ Assets/Scripts/1. Managers/UI/BuffUIManager.cs
@@ -34,10 +34,7 @@ public class BuffUIManager : MonoBehaviour
             playerChannelShieldsController.UpdateUI(channelIntShieldDict);
 
         if (character == CharacterSelect.Opponent)
-        {
             opponentChannelShieldsController.UpdateUI(channelIntShieldDict);
-            Debug.Log("Updating shields UI for " + character);
-        }
     }
 
     public void UpdateChannelShieldsFalloff(CharacterSelect character, Dictionary<Channels, List<ChannelShieldFalloffObject>> channelShieldFalloffDict)
