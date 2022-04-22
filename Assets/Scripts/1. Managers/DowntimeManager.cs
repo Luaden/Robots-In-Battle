303d1e8f40c9061c5521e92c06c1100a088f20fc
Ace Derrick Young <luadengames@gmail.com>
2022-04-22 12:29:44 +0200

    DeckBuilder/MechBuilder in Downtime

--- Assets/Scripts/1. Managers/DowntimeManager.cs
+++ Assets/Scripts/1. Managers/DowntimeManager.cs
@@ -9,12 +9,15 @@ public class DowntimeManager : MonoBehaviour
     private CardShopManager cardShopManager;
     private ComponentShopManager componentShopManager;
     private ShopCollectionRandomizeManager shopCollectionRandomizeManager;
+    private MechBuilderController mechBuilderController;
+    private DeckBuilderController deckBuilderController;
 
     public static DowntimeManager Instance { get { return instance; } }
     public CardShopManager CardShopManager { get => cardShopManager; }
     public ComponentShopManager ComponentShopManager { get => componentShopManager; }
-
     public ShopCollectionRandomizeManager ShopCollectionRandomizeManager { get => shopCollectionRandomizeManager; }
+    public MechBuilderController MechBuilderController { get => mechBuilderController; }
+    public DeckBuilderController DeckBuilderController { get => deckBuilderController; }
 
 
     private void Awake()
@@ -29,6 +32,8 @@ public class DowntimeManager : MonoBehaviour
         cardShopManager = GetComponentInChildren<CardShopManager>(true);
         componentShopManager = GetComponentInChildren<ComponentShopManager>(true);
         shopCollectionRandomizeManager = GetComponentInChildren<ShopCollectionRandomizeManager>(true);
+        mechBuilderController = GetComponent<MechBuilderController>();
+        deckBuilderController = GetComponent<DeckBuilderController>();
 
         Instance.CardShopManager.InitializeShop();
         Instance.ComponentShopManager.InitializeShop();
