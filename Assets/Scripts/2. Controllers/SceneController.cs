2d058127e1ce5fd2aa042f56bfffb03f3cfb5b08
Oliver <oliver.fahlstroem@gmail.com>
2022-04-27 12:17:24 +0200

    scenecontroller funcs
    for loading title, downtime and combat scene,

--- Assets/Scripts/2. Controllers/SceneController.cs
+++ Assets/Scripts/2. Controllers/SceneController.cs
@@ -5,9 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    public void LoadTitleScene()
     {
-       if(SceneManager.GetSceneByName(sceneName).IsValid())
-            SceneManager.LoadScene(sceneName);
+        if(SceneManager.GetSceneAt(0).IsValid())
+            SceneManager.LoadScene(0);
+    }
+    public void LoadDowntimeScene()
+    {
+        if (SceneManager.GetSceneAt(1).IsValid())
+            SceneManager.LoadScene(1);
+    }
+    public void LoadCombatScene()
+    {
+        if (SceneManager.GetSceneAt(2).IsValid())
+            SceneManager.LoadScene(2);
     }
 }
