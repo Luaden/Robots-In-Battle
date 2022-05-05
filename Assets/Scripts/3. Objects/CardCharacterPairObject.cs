62310e320e644b20ef66ff6987b0b6da57e78065
Ace Derrick Young <luadengames@gmail.com>
2022-05-05 15:53:33 +0200

    Fix For Flurry Blocking

--- Assets/Scripts/3. Objects/CardCharacterPairObject.cs
+++ Assets/Scripts/3. Objects/CardCharacterPairObject.cs
@@ -2,10 +2,12 @@ public class CardCharacterPairObject
 {
     public CardChannelPairObject cardChannelPair;
     public CharacterSelect character;
+    public int repeatEffect;
 
-    public CardCharacterPairObject(CardChannelPairObject cardChannelPair, CharacterSelect character)
+    public CardCharacterPairObject(CardChannelPairObject cardChannelPair, CharacterSelect character, int repeatEffect = 1)
     {
         this.cardChannelPair = cardChannelPair;
         this.character = character;
+        this.repeatEffect = repeatEffect;
     }
 }
